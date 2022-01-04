6a3560be94d7a5894dbda674fbc326567502ab1a
rishikeshjoshi123 <73937124+rishikeshjoshi123@users.noreply.github.com>
2022-01-04 23:50:59 +0530

    added markup, css and script

---
+++ script.js
@@ -0,0 +1,20 @@
+const deg = 6;
+//dom element id
+const hr = document.getElementById('hr');
+const mn = document.getElementById('mn');
+const sc = document.getElementById('sc');
+
+
+setInterval(() => {
+    let date = new Date();
+    //h,m,s is the degree each hand has to move 
+    let h = (date.getHours() * 30) % 360;
+    let m = (date.getMinutes() * deg) % 360;
+    let s = (date.getSeconds() * deg) % 360;
+
+    //set css transfrom (rotate) to clock hands using DOM
+    hr.style.transform = `rotateZ(${h + (m / 12)}deg)`;
+    mn.style.transform = `rotateZ(${m}deg)`;
+    sc.style.transform = `rotateZ(${s}deg)`;
+
+});
